2d1212bcc2362cf1e750b75e3aa267a3f45593b0
KANAAMI <71115536+KANAAMI2829@users.noreply.github.com>
2020-10-20 16:15:48 +0900

    Add files via upload

---
+++ LoadingAnimation_Bounce/Animation.js
@@ -0,0 +1,49 @@
+'use strict';
+
+function Bounce_Nowloading() { // 跳ねるCSSアニメーションを動かす処理
+  let id_num = 0; 
+  function replace_class_bounce() {
+    let obj = document.getElementById(`${id_num}`);
+    obj.classList.add('bounce'); // classをbounceへ0.05秒に一回置換していく処理
+    if (obj.classList.contains('def')) {
+      obj.classList.remove('def');
+    } else {
+      obj.classList.remove('reposition');
+    }
+    console.log(obj.classList);
+    id_num++;
+
+    if (id_num > 15) {
+      clearInterval(id);
+    }
+  }
+
+  let id = setInterval(() => {
+    replace_class_bounce();
+  }, 50);
+}
+
+function Reposition_Nowloading() { // 再配置のCSSアニメーションを動かす処理
+  for (let id_num = 0; id_num < 16; id_num++) { // classをbounceからrepositionへ置換していく処理
+    let obj = document.getElementById(`${id_num}`);
+    obj.classList.add('reposition');
+    obj.classList.remove('bounce');
+    console.log(obj.classList);
+  }
+}
+
+function Nowloading_Animation() {
+  Bounce_Nowloading();
+  setTimeout(() => {
+    Reposition_Nowloading();
+  }, 1800);
+
+  const Loading_Animation = setInterval(() => {
+    Bounce_Nowloading();
+    setTimeout(() => {
+      Reposition_Nowloading();
+    }, 1800);
+  }, 2700);
+}
+
+Nowloading_Animation();
